32ee6d1640fd02571e397ab515f5baf1dcb43c2c
Patrick Tumu <patricktumu@gmail.com>
2023-05-15 19:06:16 -0600

    added html to urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -26,6 +26,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: req.params.longURL};
+  console.log(req.params.id);
+  console.log(req.params.longURL);
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
