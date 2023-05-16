95aad36bba0cb48058954778b9884cde08aaa5b0
Patrick Tumu <patricktumu@gmail.com>
2023-05-15 20:39:05 -0600

    added new get and post routes to temp database obj

--- express_server.js
+++ express_server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+// program to generate random strings
+
+// declare all characters
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//this function was found @programiz.com
+const generateString = function(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 };
 
 app.get("/", (req, res) => {
@@ -26,10 +42,23 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const longURL = req.body.longURL;
+  const shortURl = generateString(6);
+  urlDatabase[shortURl] = longURL;
+  console.log(urlDatabase);
+  res.send("ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   const shortUrl = req.params.id;
-  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl]};
-  
+  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl] };
+
   res.render("urls_show", templateVars);
 });
 
