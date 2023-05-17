6876466faa85bd712449d9d5335c4c284bf6043b
Patrick Tumu <patricktumu@gmail.com>
2023-05-17 17:12:10 -0600

    added unverified login feature with display in header

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 const { generateString } = require("./generateString");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -25,7 +27,9 @@ app.get("/urls.json", (req, res) => {
 
 //show a urls_index page containing the info of the db
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,
+    username: req.cookies["username"] };
+  console.log("COOKIE :" +  req.cookies["username"]);
   res.render("urls_index", templateVars);
 });
 
@@ -36,7 +40,8 @@ app.get("/hello", (req, res) => {
 
 //display the urls_new page if put into the path
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars =  { username: req.cookies["username"]};
+  res.render("urls_new" , templateVars);
 });
 
 //catch all path after u/ where it will take either a shortUrl from the DB
@@ -97,7 +102,7 @@ app.post('/urls/:id/delete', (req, res) => {
 // page that shows longUrl for input shortUrl
 app.get("/urls/:id", (req, res) => {
   const shortUrl = req.params.id;
-  const templateVars = { id: shortUrl, longUrl: urlDatabase[shortUrl] };
+  const templateVars = { id: shortUrl, longUrl: urlDatabase[shortUrl], username: req.cookies["username"] };
 
   res.render("urls_show", templateVars);
 });
